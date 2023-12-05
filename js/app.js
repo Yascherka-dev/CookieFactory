1c14d90a22386ca4ec40cd74eba442d4c65ecd81
Yasmina CHERKAOUI <yasmina.cerkaoui@mail-esd.com>
2023-12-05 15:00:46 +0100

    cookiefactory

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const bigCookie = getElementById("bigCookie")
+const cookieValue = getElementById("cookieValue")
+const moneyValue = getElementById("moneyValue")
+const gmValue = getElementById("gmValue")
+const btnBuyGm = getElementById("btnBuyGm")
+
+
